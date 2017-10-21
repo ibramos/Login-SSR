8e81717e1af597d5f1d8942d2954802789a2b4b4
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-10-21 06:44:39 -0700

    implement react router support for server side rendering

--- src/index.js
+++ src/index.js
@@ -10,8 +10,14 @@ app.use(express.static('public'));
 
 // html generated from the renderer helper will be the template served for SEO optimization and faster loading time
 // bundle.js created by the webpack.client provides the functionality for the template
-app.get('/', (req, res) => {
-  res.send(renderer());
+
+// BrowserRouter has the ability to look directly at the browser's address bar to figure out the current path and what set of components it needs to show on the screen
+// BrowserRouter doesn't work in the server which is why StaticRouter is needed
+// StaticRouter needs to be told exactly what the current path it needs to consider which is achieved by passing the request object into the renderer helper
+
+// express serves as the middleware for all incoming HTTP requests which gets passed down to React Router to decide the outcome
+app.get('*', (req, res) => {
+  res.send(renderer(req));
 });
 
 app.listen(PORT, () => {
