e98275e2c6862db6e3f81e1180fc83df6b425e71
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-10-23 09:36:59 -0700

    add protection against cross site scripting attack

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Routes from '../client/Routes';
 import { renderRoutes } from 'react-router-config';
+import serialize from 'serialize-javascript';
+import Routes from '../client/Routes';
 
 export default (req, store) => {
   const content = renderToString(
@@ -18,11 +19,11 @@ export default (req, store) => {
 		<html>
 		  <head></head>
 		  <body>
-			<div id="root">${content}</div>
-			<script>
-				window.INITIAL_STATE = ${JSON.stringify(store.getState())}
-			</script>
-			<script src="bundle.js"></script>
+				<div id="root">${content}</div>
+				<script>
+					window.INITIAL_STATE = ${serialize(store.getState())}
+				</script>
+				<script src="bundle.js"></script>
 		  <body>
 		</html>
 	  `;
